dec23403a0b30cbe3ddb575bccd6e1a6e841319b
SohailaAbdElazeem <abdelazeemsohaila@gmail.com>
2026-04-07 00:42:57 +0200

    add global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
- import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import Layout from './components/Layout/Layout';
-import Home from './components/Home/Home';
-import SpecialtiesAndSecurity from './components/SpecialtiesAndSecurity/SpecialtiesAndSecurity';
-import SmartFeatures from './components/SmartFeatures/SmartFeatures';
-import PersonalizeExp from './components/PersonalizeExp/PersonalizeExp';
-import Quickstart from './components/Quickstart/Quickstart';
-import AccountSettings from './components/AccountSettings/AccountSettings';
-import Subscriptions from './components/Subscriptions/Subscriptions';
-import FAQ from './components/FAQ/FAQ';
-import JoinUs from './components/JoinUs/JoinUs';
-import FormPage from './components/FormPage/FormPage';
-const Router=createBrowserRouter([
-    {path:'',element:<Layout/>,children:[
-      {path:'',element:<Home/>},
-      {path:'Home',element:<Home/>},
-      {path:'specialtiesAndSecurity',element:<SpecialtiesAndSecurity/>},
-      {path:'smartFeatures',element:<SmartFeatures/>},
-      {path:'personalizeExp',element:<PersonalizeExp/>},
-      {path:'quickstart',element:<Quickstart/>},
-      {path:'accountSettings',element:<AccountSettings/>},
-      {path:'subscriptions',element:<Subscriptions/>},
-      {path:'faq',element:<FAQ/>},
-      {path:'joinUs',element:<JoinUs/>},
-      {path:'formpage',element:<FormPage/>},
-    ]}
-])
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import SpecialtiesAndSecurity from "./components/SpecialtiesAndSecurity/SpecialtiesAndSecurity";
+import SmartFeatures from "./components/SmartFeatures/SmartFeatures";
+import PersonalizeExp from "./components/PersonalizeExp/PersonalizeExp";
+import Quickstart from "./components/Quickstart/Quickstart";
+import AccountSettings from "./components/AccountSettings/AccountSettings";
+import Subscriptions from "./components/Subscriptions/Subscriptions";
+import FAQ from "./components/FAQ/FAQ";
+import JoinUs from "./components/JoinUs/JoinUs";
+import FormPage from "./components/FormPage/FormPage";
+import NewAccount from "./components/NewAccount/NewAccount";
+const Router = createBrowserRouter([
+	{
+		path: "",
+		element: <Layout />,
+		children: [
+			{ path: "", element: <Home /> },
+			{ path: "Home", element: <Home /> },
+			{ path: "specialtiesAndSecurity", element: <SpecialtiesAndSecurity /> },
+			{ path: "smartFeatures", element: <SmartFeatures /> },
+			{ path: "personalizeExp", element: <PersonalizeExp /> },
+			{ path: "quickstart", element: <Quickstart /> },
+			{ path: "accountSettings", element: <AccountSettings /> },
+			{ path: "subscriptions", element: <Subscriptions /> },
+			{ path: "FAQs", element: <FAQ /> },
+			{ path: "joinUs", element: <JoinUs /> },
+			{ path: "formpage", element: <FormPage /> },
+			{ path: "newAccount", element: <NewAccount /> },
+		],
+	},
+]);
 function App() {
-  return (
-    <div className="App">
-        <RouterProvider router={Router}></RouterProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<RouterProvider router={Router}></RouterProvider>
+		</div>
+	);
 }
 
 export default App;
