34b37ac91022a53de22cfa6e68dc143cc6f3b421
SohailaAbdElazeem <abdelazeemsohaila@gmail.com>
2026-04-07 00:45:04 +0200

    Add Header.css and update Footer/Header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,24 @@
-import "./Header.css";
+import "./Header.modules.css";
 import logo from "../../assests/logo (2).png";
 import { Link } from "react-router-dom";
 export default function Header() {
 	return (
 		<div className="main-section">
 			<div className="header">
-					{/* Logo + Text */}
 				<div className="logo-container">
 					<Link to="/">
 						<img src={logo} alt="logo" />
 					</Link>
 					<span className="line"></span>
-					<span className="logo-text">المساعدة</span>
+					<Link className="logo-text" to="/">
+						المساعدة
+					</Link>
 				</div>
 
 				{/* Translate Button */}
 				<button className="translate" id="translateBtn">
 					English
 				</button>
-			
 			</div>
 		</div>
 	);
